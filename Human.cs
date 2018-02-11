a691f6ccfabee2cc1bf33b59d8668a87d18db594
Nuh Mohamud <nuhmohamud@Nuhs-MacBook-Pro.local>
2018-02-11 01:07:24 -0800

    Creating objects

---
+++ Human.cs
@@ -0,0 +1,31 @@
+namespace first_csharp {
+    public class Human {
+        public string name {get; set;}
+        public int strength {get; set;}
+        public int intelligence {get; set;}
+        public int dexterity {get; set;}
+        public int health {get; set;}
+
+        public Human(string name) {
+            this.name = name;
+            this.strength = 3;
+            this.intelligence = 3;
+            this.dexterity = 3;
+            this.health = 100;
+        }
+        public Human(string name, int strength, int intelligence, int dexterity, int health) {
+            this.name = name;
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.dexterity = dexterity;
+            this.health = health;
+        }
+
+        public void attack(object o) {     
+            Human h = o as Human;
+            if(h != null) {
+                h.health -= (this.strength * 5);
+            }
+        }
+    }
+}
