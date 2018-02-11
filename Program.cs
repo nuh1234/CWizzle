7a2cb0b16be613053274580d2fec4f98664c0f06
Nuh Mohamud <nuhmohamud@Nuhs-MacBook-Pro.local>
2018-02-10 23:10:35 -0800

    Testing

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace first_csharp
         static void Main(string[] args)
         {
             Console.WriteLine("My first c sharp app!");
+            counter();
+        }
+
+        static void counter() {
+            for(int i = 1; i <= 255; i++) {
+                if(i % 3 == 0) {
+                    Console.WriteLine("Three {0}", i);
+                } else if(i % 5 == 0) {
+                    Console.WriteLine("Five {0}", i);
+                }
+            }
         }
     }
 }
