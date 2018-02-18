6cdb7b66f1c926fcfab3a2f6ecc3325d03aa0684
nuh1234 <nuhmohamud@Nuhs-MacBook-Pro.local>
2018-02-17 21:06:14 -0800

    untracked changess

--- Program.cs
+++ Program.cs
@@ -10,14 +10,6 @@ namespace first_csharp
             Console.WriteLine("Name {0} Health {1} Intelligence {2}", w.name, w.health, w.intelligence);
         }
 
-        static void counter() {
-            for(int i = 1; i <= 255; i++) {
-                if(i % 3 == 0) {
-                    Console.WriteLine("Three {0}", i);
-                } else if(i % 5 == 0) {
-                    Console.WriteLine("Five {0}", i);
-                }
-            }
-        }
+
     }
 }
